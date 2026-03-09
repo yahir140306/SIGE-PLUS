35c57bb4769780744120a1acc1f496ed3f362483
yahir140306 <isidrocej@gmail.com>
2026-03-09 17:53:34 -0600

    fix: remove .vercel from git tracking and add to gitignore

--- .vercel/output/_functions/chunks/supabase_DVVtauOl.mjs
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://iyfydkgtfeyxprlftvdv.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5Znlka2d0ZmV5eHBybGZ0dmR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzNDA5NTgsImV4cCI6MjA4NzkxNjk1OH0.xjdjZ-nf9y0MwdTknHVKHevFipcygtZRjbEB3TJ4ngs";
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export { supabase as s };
