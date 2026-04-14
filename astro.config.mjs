223c4a4f785bde36be8acebc7aa82f3cb6a2dfe7
yahir140306 <isidrocej@gmail.com>
2026-04-14 00:46:41 -0600

    feat: update configuration and dependencies for improved testing and performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({}),
 });
